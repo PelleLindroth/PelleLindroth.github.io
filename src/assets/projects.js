23135fe19d3a4a721682b723b266c66caa8fc266
PelleLindroth <pellelindroth@gmail.com>
2021-01-02 10:37:53 +0100

    Add logo, screenshots, icons

---
+++ src/assets/projects.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    color: "#0A78F9",
+    column: [1, 3],
+    row: [3, 4],
+    title: "Do the Todo",
+    description: "En enkel Todo-app i Vue. Använder Firebase Realtime Database för att spara användarnas todos, och Firebase Authentication för inloggning.",
+    id: 1
+  },
+  {
+    color: "#398dfa",
+    column: [1, 3],
+    row: [8, 4],
+    title: "Do be do",
+    description: "En enkel Todo-app i Vue. Använder Firebase Realtime Database för att spara användarnas todos, och Firebase Authentication för inloggning.",
+    id: 2
+  },
+  {
+    color: "yellow",
+    column: [1, 3],
+    row: [13, 4],
+    title: "Frankie",
+    description: "Loreen IPzen., Loreen IPzen., Loreen IPzen., Loreen IPzen., Loreen IPzen., Loreen IPzen., ",
+    id: 3
+  }
+]
