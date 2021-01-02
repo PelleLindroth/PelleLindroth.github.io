5b127b99e29dae191435dcbd0d0bb99214534982
PelleLindroth <pellelindroth@gmail.com>
2021-01-02 10:29:41 +0100

    Add routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import Home from '../views/Home.vue'
+import Projects from '../views/Projects.vue'
+import CV from '../views/CV.vue'
+import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -8,20 +12,28 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/projects',
+    name: 'Projects',
+    component: Projects
+  },
+  {
+    path: '/cv',
+    name: 'CV',
+    component: CV
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
+  linkExactActiveClass: 'is-active',
   base: process.env.BASE_URL,
   routes
 })
