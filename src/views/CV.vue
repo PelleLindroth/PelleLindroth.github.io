8257db3be0fdd379f542796f25df26b7f4d88732
PelleLindroth <pellelindroth@gmail.com>
2021-01-03 23:57:11 +0100

    CV view html complete and styled for desktop

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -1,6 +1,267 @@
 <template>
-  <div>
-    CV
+  <div class="cv-view-wrapper">
+    <div class="presentation">
+      <div class="presentation-header">
+        <img src="@/assets/profile-picture.png" alt="Profile picture" />
+        <h2>Om mig</h2>
+      </div>
+      <div class="presentation-content">
+        <p>
+          Jag går för närvarande första året på den 2-åriga
+          Javascriptutbildningen på IT-Högskolan i Stockholm. Utbildningen
+          innefattar förutom frontend- och backendutveckling i Javascript även
+          grafisk design och UX, crossplatform-utveckling, agila arbetsmetoder
+          och mycket annat.<br /><br />I min framtida yrkesroll hoppas jag få
+          tillfälle att arbeta både med frontend- och backend, kanske helst med
+          lutning åt det senare. Jag trivs bra när jag får bygga nya saker och
+          lösa problem och eftersom jag ägnat en stor del av livet åt musik har
+          jag lätt att se likheter mellan programmering och musikskapande. Den
+          kick jag brukade få från att skapa musik och text får jag numera
+          mestadels genom att koda... <br /><br />Jag söker nu LIA-praktik för
+          hösten 2021. Om du tror att jag skulle passa in på ditt företag,
+          <a href="/contact">hör gärna av dig</a>!
+        </p>
+      </div>
+    </div>
+    <div class="cv-container">
+      <section class="skills">
+        <h3>Kompetens</h3>
+        <div class="skills-content-boxes">
+          <div class="skills-content">
+            <p>Språk</p>
+
+            <ul>
+              <li>
+                <div class="icon-container">
+                  <img
+                    src="../assets/javascript-logo.svg"
+                    alt="Javascript Logo"
+                  />
+                </div>
+                Javascript (ES6)
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/java-logo.svg" alt="Java Logo" />
+                </div>
+                Java
+              </li>
+
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/html-5-logo.svg" alt="HTML 5 Logo" />
+                </div>
+                HTML 5
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/css-3-logo.svg" alt="CSS 3 Logo" />
+                </div>
+                CSS 3
+              </li>
+            </ul>
+            <p>Bibliotek och ramverk</p>
+
+            <ul>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/react-logo.svg" alt="React Logo" />
+                </div>
+                React
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/redux-logo.svg" alt="Redux Logo" />
+                </div>
+                Redux
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/vue-logo.svg" alt="Vue Logo" />
+                </div>
+                Vue
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/sass-logo.svg" alt="Sass Logo" />
+                </div>
+                SASS
+              </li>
+            </ul>
+            <p>Backend</p>
+
+            <ul>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/nodejs-logo.svg" alt="Node.js Logo" />
+                </div>
+                Node.js (Express)
+              </li>
+            </ul>
+          </div>
+          <div class="skills-content">
+            <p>Databashantering & autentisering</p>
+
+            <ul>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/mongodb-logo.png" alt="MongoDB Logo" />
+                </div>
+                MongoDB
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/firebase-logo.svg" alt="Firebase Logo" />
+                </div>
+                Google Firebase
+              </li>
+            </ul>
+            <p>Mjukvara</p>
+
+            <ul>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/vscode-logo.svg" alt="VS Code Logo" />
+                </div>
+                VS Code
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img
+                    src="../assets/intellij-idea-logo.svg"
+                    alt="IntelliJ IDEA Logo"
+                  />
+                </div>
+                IntelliJ IDEA
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/netbeans-logo.svg" alt="Netbeans Logo" />
+                </div>
+                Netbeans
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/postman-icon.svg" alt="Postman Logo" />
+                </div>
+                Postman
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/figma-logo.svg" alt="Figma Logo" />
+                </div>
+                Figma
+              </li>
+              <li>
+                <div class="icon-container">
+                  <img
+                    src="../assets/mongodb-logo.png"
+                    alt="MongoDB Compass Logo"
+                  />
+                </div>
+                MongoDB Compass
+              </li>
+            </ul>
+
+            <p>Testning</p>
+
+            <ul>
+              <li>
+                <div class="icon-container">
+                  <img src="../assets/jest-logo.svg" alt="Jest Logo" />
+                </div>
+                Jest
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section class="jobs">
+        <h3>Tidigare jobb (i urval)</h3>
+        <div class="job-boxes">
+          <div class="job-box">
+            <p class="blue"><span>Vad: </span>Copywriter/ SEO writer</p>
+            <p class="blue"><span>Var: </span>Defiso Media</p>
+            <p class="blue"><span>När: </span>2018-2020</p>
+            <hr />
+            <p class="job-description">
+              Skrev content till kunders landningssidor och till deras egna
+              hemsidor. I jobbet ingick mycket research, viss kundkontakt,
+              design och publicering via CMS samt såklart en hel del skrivande.
+            </p>
+          </div>
+          <div class="job-box">
+            <p class="red"><span>Vad: </span>Kompositörsuppdrag</p>
+            <p class="red"><span>Var: </span>Uppsala Stadsteater</p>
+            <p class="red"><span>När: </span>2018</p>
+            <hr />
+            <p class="job-description">
+              Skrev musik och texter till föreställningen Uppgång & Fall på
+              Uppsala Stadsteater. Jobbet innefattade även inspelning av musiken
+              och sångrep med skådespelarna.
+            </p>
+          </div>
+          <div class="job-box">
+            <p class="green"><span>Vad: </span>Aktivitetscoach / vårdare</p>
+            <p class="green">
+              <span>Var: </span>Stockholms kommun / Frösunda LSS
+            </p>
+            <p class="green"><span>När: </span>Periodvis 2001 - 2017</p>
+            <hr />
+            <p class="job-description">
+              Har haft flera olika arbeten inom LSS, både på gruppboenden och i
+              daglig verksamhet. Förutom rena omsorgstjänster har jobbet
+              innefattat fysiskt rehab- och terapiarbete, arbete med alternativa
+              kommunikationsformer och handledning i exempelvis musik och
+              teater.
+            </p>
+          </div>
+          <div class="job-box">
+            <p class="cyan">
+              <span>Vad: </span>Musiker, sångare och låtskivare
+            </p>
+            <p class="cyan"><span>Var: </span>Frilans</p>
+            <p class="cyan"><span>När: </span>1992-</p>
+            <hr />
+            <p class="job-description">
+              Skrivit, spelat in och släppt musik och turnerat med en rad
+              grupper som Moder Jords Massiva, Hynek, The Chrysler, Liljor och
+              med soloprojektet Parken.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="education">
+        <h3>Utbildning</h3>
+        <div class="education-boxes">
+          <div class="education-box">
+            <p><span>Vad: </span>Javascriptutveckling</p>
+            <p><span>Var: </span>IT-Högskolan i Stockholm</p>
+            <p><span>När: </span>Aug 2020 - Jun 2022</p>
+          </div>
+          <div class="education-box">
+            <p><span>Vad: </span>Object-oriented programming with Java 1 & 2</p>
+            <p><span>Var: </span>Helsinki University (online)</p>
+            <p><span>När: </span>Dec 2019 - Feb 2020</p>
+          </div>
+          <div class="education-box">
+            <p><span>Vad: </span>Historia</p>
+            <p><span>Var: </span>Södertörns Högskola</p>
+            <p><span>När: </span>Jan 2003 - Maj 2003</p>
+          </div>
+          <div class="education-box">
+            <p><span>Vad: </span>Musikvetenskap</p>
+            <p><span>Var: </span>Stockholms Universitet</p>
+            <p><span>När: </span>Aug 2000 - Jun 2001</p>
+          </div>
+          <div class="education-box">
+            <p><span>Vad: </span>Engelska och engelsk litteratur</p>
+            <p><span>Var: </span>Stockholms Universitet</p>
+            <p><span>När: </span>Aug 2001 - Jun 2002</p>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -12,4 +273,179 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+$mobile-cutoff: 650px;
+
+.cv-view-wrapper {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(20, 64px);
+  margin: 4rem auto 0;
+  max-width: 120rem;
+
+  @media screen and (max-width: $mobile-cutoff) {
+    gap: 1rem;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(20, 1.6rem);
+    margin: auto;
+    padding: 0 1rem;
+    width: 100vw;
+    min-height: 100vh;
+  }
+
+  .presentation {
+    grid-column: 1 / span 7;
+    grid-row: 1 / span 7;
+
+    .presentation-header {
+      align-items: flex-end;
+      display: flex;
+      margin-bottom: 2rem;
+
+      img {
+        width: 14rem;
+      }
+
+      h2 {
+        color: #fcd577;
+        font-family: 'Righteous', cursive;
+        font-size: 3.2rem;
+      }
+    }
+
+    .presentation-content {
+      font-size: 2.2rem;
+      font-weight: 300;
+
+      a {
+        color: #ececec;
+        cursor: pointer;
+        font-weight: 400;
+        text-decoration: none;
+      }
+    }
+  }
+
+  section {
+    margin-bottom: 10rem;
+  }
+
+  .cv-container {
+    display: flex;
+    flex-direction: column;
+    grid-column: 1 / span 12;
+    grid-row: 10 / auto;
+
+    h3 {
+      background-color: #fcd577;
+      color: #333;
+      font-family: 'Righteous', cursive;
+      font-size: 3rem;
+      margin-bottom: 3rem;
+      padding: 0.5rem 0 0.5rem 1rem;
+      width: 100%;
+    }
+
+    .skills-content-boxes {
+      display: flex;
+
+      .skills-content {
+        margin-right: 5rem;
+        min-width: 50rem;
+
+        p {
+          font-size: 2.6rem;
+          font-weight: 700;
+          margin-bottom: 1rem;
+        }
+
+        ul {
+          list-style: none;
+          margin-bottom: 3rem;
+        }
+
+        li {
+          align-items: center;
+          display: flex;
+          font-size: 2.4rem;
+          font-weight: 300;
+          line-height: 1.2;
+          margin-bottom: 0.5rem;
+
+          .icon-container {
+            display: flex;
+            justify-content: flex-start;
+            min-width: 4.5rem;
+
+            img {
+              height: 2.4rem;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .jobs {
+    .job-boxes {
+      display: flex;
+      flex-wrap: wrap;
+
+      .job-box {
+        font-size: 2.4rem;
+        font-weight: 300;
+        margin-bottom: 5rem;
+        margin-right: 5rem;
+        max-width: 50rem;
+
+        hr {
+          border: 1px solid #333;
+          margin-bottom: 1rem;
+          margin-top: 0.5rem;
+        }
+
+        .blue {
+          color: #0a78f9;
+        }
+        .red {
+          color: #f53f42;
+        }
+        .green {
+          color: #008000;
+        }
+        .cyan {
+          color: #3298af;
+        }
+
+        span {
+          font-weight: 700;
+        }
+      }
+    }
+  }
+
+  .education {
+    padding-bottom: 30rem;
+
+    .education-boxes {
+      display: flex;
+      flex-wrap: wrap;
+
+      .education-box {
+        margin-bottom: 5rem;
+        margin-right: 5rem;
+        min-width: 50rem;
+        p {
+          font-size: 2.4rem;
+          font-weight: 300;
+        }
+
+        span {
+          font-weight: 700;
+        }
+      }
+    }
+  }
+}
+</style>
