b538219db7b2fcce488877d659004e5340c29aa0
PelleLindroth <pellelindroth@gmail.com>
2021-01-03 18:27:12 +0100

    Updated projects with links

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,35 +2,37 @@ export default [
   {
     color: "#0A78F9",
     description: "En enkel Todo-app i Vue. Använder Firebase Realtime Database för att spara användarnas todos, och Firebase Authentication för inloggning.",
-    id: 1,
+    id: 0,
     images: ['todos-screenshot-1.png', 'todos-screenshot-2.png'],
-    links: [],
-    row: [3, 4],
+    links: ['https://github.com/PelleLindroth/do-the-todo', 'https://pellelindroth.github.io/do-the-todo/'],
+    row: 3,
     title: "Do the Todo",
   },
   {
     color: '#FFD700',
     description: 'Min version av Snake. Native JS, använde classes för att få tydligare kodstruktur och lärde mig om canvas. Funkar bara på desktop än så länge.',
-    id: 2,
+    id: 1,
     images: ['snake-screenshot.png'],
-    row: [8, 4],
+    links: ['https://github.com/PelleLindroth/snake', 'https://pellelindroth.github.io/snake/'],
+    row: 8,
     title: 'Snake'
   },
   {
     color: '#008000',
     description: 'Uppgiften var att skriva en enkel assembler-tolkare som accepterade ett urval kommandon och utförde dem. Har även en debugger!',
-    id: 3,
+    id: 2,
     images: ['assembly-interpreter-screenshot-1.png', 'assembly-interpreter-screenshot-2.png', 'assembly-interpreter-screenshot-3.png'],
-    row: [13, 4],
+    links: ['https://github.com/PelleLindroth/interpreter', 'https://pellelindroth.github.io/interpreter/'],
+    row: 13,
     title: 'Assembly Interpreter'
   },
   {
     color: '#F53F42',
-    description: 'Ett spel jag byggde under tiden jag började lära mig Javascript, Node och React. Den gamla hederliga bandleken i browserversion. Använder Discogs API och spar band och highscores i en databas på MongoDB Atlas.',
-    id: 4,
+    description: 'Ett spel jag byggde medan jag började lära mig Javascript, Node och React. Den gamla hederliga bandleken i browserversion. Använder <a target="_blank" href="https://www.discogs.com/developers" style="color: #ececec; text-decoration: none; font-weight: 400">Discogs API</a> och sparar band och highscores med MongoDB Atlas.',
+    id: 3,
     images: ['bandfeud-screenshot-1.png', 'bandfeud-screenshot-2.png', 'bandfeud-screenshot-3.png', 'bandfeud-screenshot-4.png'],
-    links: [],
-    row: [18, 4],
+    links: ['https://github.com/Pellin/bandfeud', 'https://bandfeud.herokuapp.com/'],
+    row: 18,
     title: 'Bandfeud'
   },
 ]
