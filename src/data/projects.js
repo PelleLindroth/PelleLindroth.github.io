4f3976db05803f920d6067ac6e7fa18b40f381ea
PelleLindroth <pellelindroth@gmail.com>
2021-01-15 23:42:06 +0100

    Rearrange projects, change images

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,38 +1,38 @@
 export default [
   {
-    color: "#0A78F9",
-    description: "Skoluppgift. En enkel Todo-app i Vue. Använder Firebase Realtime Database för att spara användarnas todos, och Firebase Authentication för inloggning.",
-    id: 0,
-    images: ['todos-screenshot-1.png', 'todos-screenshot-2.png'],
-    links: ['https://github.com/PelleLindroth/do-the-todo', 'https://pellelindroth.github.io/do-the-todo/'],
-    row: 3,
-    title: "Do the Todo",
-  },
-  {
     color: '#FFD700',
     description: 'Min version av Snake. Native JS, använde classes för att få tydligare kodstruktur och lärde mig om canvas.',
     id: 1,
-    images: ['snake-screenshot.png'],
+    images: ['phone-snake-1.png', 'phone-snake-2.png', 'phone-snake-3.png'],
     links: ['https://github.com/PelleLindroth/snake', 'https://pellelindroth.github.io/snake/'],
-    row: 8,
+    row: 3,
     title: 'Snake'
+  },
+  {
+    color: '#F53F42',
+    description: 'Ett spel jag byggde medan jag började lära mig Javascript, Node och React. Den gamla hederliga bandleken i browserversion. Använder <a target="_blank" href="https://www.discogs.com/developers" style="color: #ececec; text-decoration: none; font-weight: 400">Discogs API</a> och sparar band och highscores med MongoDB Atlas.',
+    id: 2,
+    images: ['phone-bandfeud-1.png', 'phone-bandfeud-2.png', 'phone-bandfeud-3.png', 'phone-bandfeud-4.png'],
+    links: ['https://github.com/Pellin/bandfeud', 'https://bandfeud.herokuapp.com/'],
+    row: 8,
+    title: 'Bandfeud'
   },
   {
     color: '#008000',
     description: 'Uppgiften var att skriva en enkel assembler-tolkare som accepterade ett urval kommandon och utförde dem. Har även en debugger!',
-    id: 2,
-    images: ['assembly-interpreter-screenshot-1.png', 'assembly-interpreter-screenshot-2.png', 'assembly-interpreter-screenshot-3.png'],
+    id: 3,
+    images: ['phone-interpreter-1.png', 'phone-interpreter-2.png'],
     links: ['https://github.com/PelleLindroth/interpreter', 'https://pellelindroth.github.io/interpreter/'],
     row: 13,
     title: 'Assembly Interpreter'
   },
   {
-    color: '#F53F42',
-    description: 'Ett spel jag byggde medan jag började lära mig Javascript, Node och React. Den gamla hederliga bandleken i browserversion. Använder <a target="_blank" href="https://www.discogs.com/developers" style="color: #ececec; text-decoration: none; font-weight: 400">Discogs API</a> och sparar band och highscores med MongoDB Atlas.',
-    id: 3,
-    images: ['bandfeud-screenshot-1.png', 'bandfeud-screenshot-2.png', 'bandfeud-screenshot-3.png', 'bandfeud-screenshot-4.png'],
-    links: ['https://github.com/Pellin/bandfeud', 'https://bandfeud.herokuapp.com/'],
+    color: "#0A78F9",
+    description: "Skoluppgift. En enkel Todo-app i Vue. Använder Firebase Realtime Database för att spara användarnas todos, och Firebase Authentication för inloggning.",
+    id: 4,
+    images: ['phone-todo-1.png', 'phone-todo-2.png'],
+    links: ['https://github.com/PelleLindroth/do-the-todo', 'https://pellelindroth.github.io/do-the-todo/'],
     row: 18,
-    title: 'Bandfeud'
+    title: "Do the Todo",
   },
 ]
