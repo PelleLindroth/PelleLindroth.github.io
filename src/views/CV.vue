6baec7b63d240553f49be8c92adcc450cf9a6836
PelleLindroth <pellelindroth@gmail.com>
2021-01-16 13:38:00 +0100

    Change styles on h3:s. Refactor

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="cv-view-wrapper">
-    <div class="presentation">
-      <div class="presentation-header">
-        <div class="img-container">
-          <img src="@/assets/profile-picture.png" alt="Profile picture" />
-        </div>
-        <h2>Om mig</h2>
-      </div>
-      <div class="presentation-content">
+    <div class="presentation-container">
+      <div class="presentation">
         <p>
+          <img src="@/assets/profile-picture.png" alt="Profile picture" />
           Jag går för närvarande första året på den 2-åriga
           Javascriptutbildningen på IT-Högskolan i Stockholm. Utbildningen
           innefattar förutom frontend- och backendutveckling i Javascript även
@@ -23,11 +18,16 @@
           hösten 2021. Om du tror att jag skulle passa in på ditt företag,
           <router-link to="/contact">hör gärna av dig</router-link>!
         </p>
+        <img
+          class="arrow"
+          src="@/assets/icons/arrow-down.svg"
+          alt="Arrow down"
+        />
       </div>
     </div>
     <div class="cv-container">
       <section class="skills">
-        <h3>Kompetens</h3>
+        <h3><em>KOMPETENS</em></h3>
         <div class="skills-content-boxes">
           <div class="skills-content">
             <p>Språk</p>
@@ -36,7 +36,7 @@
               <li>
                 <div class="icon-container">
                   <img
-                    src="../assets/javascript-logo.svg"
+                    src="../assets/tech-logos/javascript-logo.svg"
                     alt="Javascript Logo"
                   />
                 </div>
@@ -44,20 +44,29 @@
               </li>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/java-logo.svg" alt="Java Logo" />
+                  <img
+                    src="../assets/tech-logos/java-logo.svg"
+                    alt="Java Logo"
+                  />
                 </div>
                 Java
               </li>
 
               <li>
                 <div class="icon-container">
-                  <img src="../assets/html-5-logo.svg" alt="HTML 5 Logo" />
+                  <img
+                    src="../assets/tech-logos/html-5-logo.svg"
+                    alt="HTML 5 Logo"
+                  />
                 </div>
                 HTML 5
               </li>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/css-3-logo.svg" alt="CSS 3 Logo" />
+                  <img
+                    src="../assets/tech-logos/css-3-logo.svg"
+                    alt="CSS 3 Logo"
+                  />
                 </div>
                 CSS 3
               </li>
@@ -67,25 +76,34 @@
             <ul>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/react-logo.svg" alt="React Logo" />
+                  <img
+                    src="../assets/tech-logos/react-logo.svg"
+                    alt="React Logo"
+                  />
                 </div>
                 React
               </li>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/redux-logo.svg" alt="Redux Logo" />
+                  <img
+                    src="../assets/tech-logos/redux-logo.svg"
+                    alt="Redux Logo"
+                  />
                 </div>
                 Redux
               </li>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/vue-logo.svg" alt="Vue Logo" />
+                  <img src="../assets/tech-logos/vue-logo.svg" alt="Vue Logo" />
                 </div>
                 Vue
               </li>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/sass-logo.svg" alt="Sass Logo" />
+                  <img
+                    src="../assets/tech-logos/sass-logo.svg"
+                    alt="Sass Logo"
+                  />
                 </div>
                 SASS
               </li>
@@ -95,7 +113,10 @@
             <ul>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/nodejs-logo.svg" alt="Node.js Logo" />
+                  <img
+                    src="../assets/tech-logos/nodejs-logo.svg"
+                    alt="Node.js Logo"
+                  />
                 </div>
                 Node.js (Express)
               </li>
@@ -107,13 +128,19 @@
             <ul>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/mongodb-logo.png" alt="MongoDB Logo" />
+                  <img
+                    src="../assets/tech-logos/mongodb-logo.png"
+                    alt="MongoDB Logo"
+                  />
                 </div>
                 MongoDB
               </li>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/firebase-logo.svg" alt="Firebase Logo" />
+                  <img
+                    src="../assets/tech-logos/firebase-logo.svg"
+                    alt="Firebase Logo"
+                  />
                 </div>
                 Google Firebase
               </li>
@@ -123,14 +150,17 @@
             <ul>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/vscode-logo.svg" alt="VS Code Logo" />
+                  <img
+                    src="../assets/tech-logos/vscode-logo.svg"
+                    alt="VS Code Logo"
+                  />
                 </div>
                 VS Code
               </li>
               <li>
                 <div class="icon-container">
                   <img
-                    src="../assets/intellij-idea-logo.svg"
+                    src="../assets/tech-logos/intellij-idea-logo.svg"
                     alt="IntelliJ IDEA Logo"
                   />
                 </div>
@@ -138,26 +168,35 @@
               </li>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/netbeans-logo.svg" alt="Netbeans Logo" />
+                  <img
+                    src="../assets/tech-logos/netbeans-logo.svg"
+                    alt="Netbeans Logo"
+                  />
                 </div>
                 Netbeans
               </li>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/postman-icon.svg" alt="Postman Logo" />
+                  <img
+                    src="../assets/tech-logos/postman-icon.svg"
+                    alt="Postman Logo"
+                  />
                 </div>
                 Postman
               </li>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/figma-logo.svg" alt="Figma Logo" />
+                  <img
+                    src="../assets/tech-logos/figma-logo.svg"
+                    alt="Figma Logo"
+                  />
                 </div>
                 Figma
               </li>
               <li>
                 <div class="icon-container">
                   <img
-                    src="../assets/mongodb-logo.png"
+                    src="../assets/tech-logos/mongodb-logo.png"
                     alt="MongoDB Compass Logo"
                   />
                 </div>
@@ -170,7 +209,10 @@
             <ul>
               <li>
                 <div class="icon-container">
-                  <img src="../assets/jest-logo.svg" alt="Jest Logo" />
+                  <img
+                    src="../assets/tech-logos/jest-logo.svg"
+                    alt="Jest Logo"
+                  />
                 </div>
                 Jest
               </li>
@@ -179,13 +221,14 @@
         </div>
       </section>
       <section class="jobs">
-        <h3>Tidigare jobb (i urval)</h3>
+        <h3><em>TIDIGARE JOBB (I URVAL)</em></h3>
         <div class="job-boxes">
           <div class="job-box">
-            <p class="blue"><span>Vad: </span>Copywriter/ SEO writer</p>
-            <p class="blue"><span>Var: </span>Defiso Media</p>
-            <p class="blue"><span>När: </span>2018-2020</p>
+            <h4>Copywriter / SEO writer</h4>
             <hr />
+            <p><span>Var: </span>Defiso Media</p>
+            <p><span>När: </span>2018-2020</p>
+            <hr class="invisible" />
             <p class="job-description">
               Skrev content till kunders landningssidor och till deras egna
               hemsidor. I jobbet ingick mycket research, viss kundkontakt,
@@ -193,10 +236,11 @@
             </p>
           </div>
           <div class="job-box">
-            <p class="red"><span>Vad: </span>Kompositörsuppdrag</p>
-            <p class="red"><span>Var: </span>Uppsala Stadsteater</p>
-            <p class="red"><span>När: </span>2018</p>
+            <h4>Kompositörsuppdrag</h4>
             <hr />
+            <p><span>Var: </span>Uppsala Stadsteater</p>
+            <p><span>När: </span>2018</p>
+            <hr class="invisible" />
             <p class="job-description">
               Skrev musik och texter till föreställningen Uppgång & Fall på
               Uppsala Stadsteater. Jobbet innefattade även inspelning av musiken
@@ -204,12 +248,11 @@
             </p>
           </div>
           <div class="job-box">
-            <p class="green"><span>Vad: </span>Aktivitetscoach / vårdare</p>
-            <p class="green">
-              <span>Var: </span>Stockholms kommun / Frösunda LSS
-            </p>
-            <p class="green"><span>När: </span>Periodvis 2001 - 2017</p>
+            <h4>Aktivitetscoach / vårdare</h4>
             <hr />
+            <p><span>Var: </span>Stockholms kommun / Frösunda LSS</p>
+            <p><span>När: </span>Periodvis 2001 - 2017</p>
+            <hr class="invisible" />
             <p class="job-description">
               Har haft flera olika arbeten inom LSS, både på gruppboenden och i
               daglig verksamhet. Förutom rena omsorgstjänster har jobbet
@@ -219,12 +262,11 @@
             </p>
           </div>
           <div class="job-box">
-            <p class="cyan">
-              <span>Vad: </span>Musiker, sångare och låtskivare
-            </p>
-            <p class="cyan"><span>Var: </span>Frilans</p>
-            <p class="cyan"><span>När: </span>1992-</p>
+            <h4>Musiker</h4>
             <hr />
+            <p><span>Var: </span>Frilans</p>
+            <p><span>När: </span>1992-</p>
+            <hr class="invisible" />
             <p class="job-description">
               Skrivit, spelat in och släppt musik och turnerat med en rad
               grupper som Moder Jords Massiva, Hynek, The Chrysler, Liljor och
@@ -234,30 +276,35 @@
         </div>
       </section>
       <section class="education">
-        <h3>Utbildningar</h3>
+        <h3><em>UTBILDNINGAR</em></h3>
         <div class="education-boxes">
           <div class="education-box">
-            <p><span>Vad: </span>Javascriptutveckling</p>
+            <h4>Javascriptutveckling</h4>
+            <hr />
             <p><span>Var: </span>IT-Högskolan i Stockholm</p>
             <p><span>När: </span>Aug 2020 - Jun 2022</p>
           </div>
           <div class="education-box">
-            <p><span>Vad: </span>Object-oriented programming with Java 1 & 2</p>
-            <p><span>Var: </span>Helsinki University (online)</p>
+            <h4>OOP with Java 1 & 2</h4>
+            <hr />
+            <p><span>Var: </span>Helsingfors Universitet (online)</p>
             <p><span>När: </span>Dec 2019 - Feb 2020</p>
           </div>
           <div class="education-box">
-            <p><span>Vad: </span>Historia</p>
+            <h4>Historia</h4>
+            <hr />
             <p><span>Var: </span>Södertörns Högskola</p>
             <p><span>När: </span>Jan 2003 - Maj 2003</p>
           </div>
           <div class="education-box">
-            <p><span>Vad: </span>Musikvetenskap</p>
+            <h4>Musikvetenskap</h4>
+            <hr />
             <p><span>Var: </span>Stockholms Universitet</p>
             <p><span>När: </span>Aug 2000 - Jun 2001</p>
           </div>
           <div class="education-box">
-            <p><span>Vad: </span>Engelska och engelsk litteratur</p>
+            <h4>Engelska och engelsk litteratur</h4>
+            <hr />
             <p><span>Var: </span>Stockholms Universitet</p>
             <p><span>När: </span>Aug 2001 - Jun 2002</p>
           </div>
@@ -270,7 +317,7 @@
 <script>
 export default {
   beforeCreate() {
-    document.title = 'Pelle Lindroth | CV'
+    document.title = 'Pelle Lindroth | Om mig'
   },
 }
 </script>
@@ -282,21 +329,25 @@ $mobile-cutoff: 750px;
   display: grid;
   gap: 1rem;
   grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(40, 64px);
+  grid-template-rows: repeat(44, 64px);
   margin: 4rem auto 0;
   max-width: 120rem;
+
+  @media screen and (max-width: 1150px) and (min-width: 651px) {
+    grid-template-rows: repeat(52, 64px);
+  }
 
   @media screen and (max-width: $mobile-cutoff) {
     gap: 1rem;
     grid-template-columns: repeat(8, 1fr);
-    grid-template-rows: repeat(112, 1.6rem);
+    grid-template-rows: repeat(120, 1.6rem);
     margin: auto;
     padding: 0 1rem;
     width: 100vw;
     min-height: 100vh;
   }
 
-  .presentation {
+  .presentation-container {
     grid-column: 1 / span 7;
     grid-row: 1 / span 7;
 
@@ -306,42 +357,20 @@ $mobile-cutoff: 750px;
       margin-right: 2rem;
     }
 
-    .presentation-header {
-      align-items: flex-end;
-      display: flex;
-      margin-bottom: 2rem;
-
-      .img-container {
-        min-height: 14rem;
-
-        @media screen and (max-width: $mobile-cutoff) {
-          min-height: 5rem;
-        }
-
-        img {
-          width: 14rem;
-
-          @media screen and (max-width: $mobile-cutoff) {
-            width: 5rem;
-          }
-        }
-      }
-
-      h2 {
-        color: #fcd577;
-        // font-family: 'Righteous', cursive;
-        font-family: 'Kanit', sans-serif;
-        font-size: 3.2rem;
-
-        @media screen and (max-width: $mobile-cutoff) {
-          font-size: 1.6rem;
-        }
-      }
-    }
-
-    .presentation-content {
+    .presentation {
       font-size: 2.2rem;
       font-weight: 300;
+
+      img {
+        float: left;
+        margin-right: 1.5rem;
+        shape-outside: circle(50%);
+        width: 14rem;
+
+        @media screen and (max-width: $mobile-cutoff) {
+          width: 6rem;
+        }
+      }
 
       @media screen and (max-width: $mobile-cutoff) {
         font-size: 1.5rem;
@@ -352,6 +381,16 @@ $mobile-cutoff: 750px;
         cursor: pointer;
         font-weight: 400;
         text-decoration: none;
+      }
+
+      .arrow {
+        margin-top: 4rem;
+        width: 10rem;
+
+        @media screen and (max-width: $mobile-cutoff) {
+          margin: 1.5rem 0 0 calc(50% - 2.5rem);
+          width: 5rem;
+        }
       }
     }
   }
@@ -376,19 +415,55 @@ $mobile-cutoff: 750px;
     }
 
     h3 {
-      background-color: #fcd577;
-      color: #333;
-      // font-family: 'Righteous', cursive;
+      background-color: rgba(255, 2555, 255, 0.1);
+      color: #fcd577;
       font-family: 'Kanit', sans-serif;
-      font-size: 3rem;
-      margin-bottom: 3rem;
-      padding: 0.5rem 0 0.5rem 1rem;
-      width: 100%;
+      font-size: 3.5rem;
+      font-weight: 700;
+      letter-spacing: 1.3px;
+      margin-bottom: 4rem;
+      padding-left: 1rem;
+      text-shadow: 0 0 1rem #fcd577;
+
+      @media screen and (max-width: $mobile-cutoff) {
+        font-size: 2rem;
+        letter-spacing: 0.2px;
+        margin-bottom: 3rem;
+      }
+    }
+
+    h4 {
+      font-size: 2.8rem;
+      margin-bottom: 0.5rem;
+      text-shadow: 0px 0px 4px #fff;
 
       @media screen and (max-width: $mobile-cutoff) {
         font-size: 1.8rem;
-        padding: 1rem 0 1rem 1rem;
       }
+    }
+
+    p {
+      font-size: 2.4rem;
+      font-weight: 300;
+
+      @media screen and (max-width: $mobile-cutoff) {
+        font-size: 1.5rem;
+      }
+    }
+
+    hr {
+      border: 1px solid #fff;
+      margin-bottom: 1rem;
+      margin-top: 0.5rem;
+
+      &.invisible {
+        border: 1px solid #333;
+        margin-bottom: 0.5rem;
+      }
+    }
+
+    span {
+      font-weight: 700;
     }
 
     .skills-content-boxes {
@@ -412,6 +487,7 @@ $mobile-cutoff: 750px;
           font-size: 2.6rem;
           font-weight: 700;
           margin-bottom: 1rem;
+          text-shadow: 0px 0px 3px #fff;
 
           @media screen and (max-width: $mobile-cutoff) {
             font-size: 1.6rem;
@@ -431,6 +507,7 @@ $mobile-cutoff: 750px;
           font-weight: 300;
           line-height: 1.2;
           margin-bottom: 0.5rem;
+          padding-left: 1rem;
 
           @media screen and (max-width: $mobile-cutoff) {
             font-size: 1.4rem;
@@ -475,29 +552,6 @@ $mobile-cutoff: 750px;
           font-size: 1.5rem;
           max-width: 100%;
         }
-
-        hr {
-          border: 1px solid #333;
-          margin-bottom: 1rem;
-          margin-top: 0.5rem;
-        }
-
-        .blue {
-          color: #0a78f9;
-        }
-        .red {
-          color: #f53f42;
-        }
-        .green {
-          color: #008000;
-        }
-        .cyan {
-          color: #3298af;
-        }
-
-        span {
-          font-weight: 700;
-        }
       }
     }
   }
@@ -522,19 +576,6 @@ $mobile-cutoff: 750px;
           margin-bottom: 3rem;
           min-width: 0;
           max-width: 100%;
-        }
-
-        p {
-          font-size: 2.4rem;
-          font-weight: 300;
-
-          @media screen and (max-width: $mobile-cutoff) {
-            font-size: 1.5rem;
-          }
-        }
-
-        span {
-          font-weight: 700;
         }
       }
     }
