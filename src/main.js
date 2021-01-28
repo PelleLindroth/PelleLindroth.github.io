2391cec2315d55a81a520380016fdba0afebe217
PelleLindroth <pellelindroth@gmail.com>
2021-01-28 19:03:18 +0100

    Install i18n

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import i18n from './i18n'
 
 
 Vue.config.productionTip = false
@@ -14,11 +15,7 @@ new Vue({
       )
     }
   },
-  methods: {
-    toggleLanguage() {
-      this.english = !this.english
-    }
-  },
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
